1232d4a507ee860200698de05310a157fde93f0e
Quazie Kwaselow <quazie@gmail.com>
2022-05-01 12:50:43 -0700

    Posts Container knows about the bottom of the page

--- kickposter/src/components/PostsContainer/PostsContainer.tsx
+++ kickposter/src/components/PostsContainer/PostsContainer.tsx
@@ -1,15 +1,32 @@
-import './PostsContainer.css';
-import PostBox from '../Post/Post'
-import { Post } from '../../types';
+import "./PostsContainer.css";
+import PostBox from "../Post/Post";
+import { Post } from "../../types";
+import { useEffect } from "react";
+
+function HasScrolledToBottomOfPage(event: Event) {
+  const closeToBottomBuffer = 20;
+  if (
+    window.innerHeight + window.pageYOffset >=
+    document.body.offsetHeight - closeToBottomBuffer
+  ) {
+    console.log("At bottom of page");
+  }
+}
 
 function PostsContainer() {
-  const posts = JSON.parse(localStorage.getItem("posts")!!)
-    .map((post: Post) => PostBox(post))
+  useEffect(() => {
+    window.addEventListener("scroll", HasScrolledToBottomOfPage);
+    return () => {
+      window.removeEventListener("scroll", HasScrolledToBottomOfPage);
+    };
+  });
+
+  const posts = JSON.parse(localStorage.getItem("posts")!!).map((post: Post) =>
+    PostBox(post)
+  );
   return (
     <div className="Posts-container">
-      <div className="Posts-title">
-        Your feed
-      </div>
+      <div className="Posts-title">Your feed</div>
       {posts}
     </div>
   );
